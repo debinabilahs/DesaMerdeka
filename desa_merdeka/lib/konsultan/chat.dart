61a297c5ab131d9671b2f12e3b667731d293cd3b
kaniaisn <115695101+kaniaisn@users.noreply.github.com>
2023-05-19 23:16:39 +0700

    tambah

--- desa_merdeka/lib/konsultan/chat.dart
+++ desa_merdeka/lib/konsultan/chat.dart
@@ -40,6 +40,60 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          automaticallyImplyLeading: false,
+          backgroundColor: Colors.white,
+          flexibleSpace: SafeArea(
+            child: Container(
+              padding: EdgeInsets.only(right: 16),
+              child: Row(
+                children: <Widget>[
+                  IconButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    icon: Icon(
+                      Icons.arrow_back,
+                      color: Colors.black,
+                    ),
+                  ),
+                  SizedBox(
+                    width: 2,
+                  ),
+                  CircleAvatar(
+                    backgroundImage: NetworkImage(
+                        "<https://randomuser.me/api/portraits/men>"),
+                    maxRadius: 20,
+                  ),
+                  SizedBox(
+                    width: 12,
+                  ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          "Desa A",
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.w600),
+                        ),
+                        SizedBox(
+                          height: 6,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Icon(
+                    Icons.book,
+                    color: Colors.black54,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
         body: Chat(
           messages: _messages,
           onAttachmentPressed: _handleAttachmentPressed,
